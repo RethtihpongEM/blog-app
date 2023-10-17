d40ea66d5387a148bf5186b7ab4c0d7e2eb69689
RethtihpongEM <rethtihpong@gmail.com>
2023-10-17 23:06:25 +0700

    add mongodb

--- server.js
+++ server.js
@@ -1,34 +1,43 @@
+require("dotenv").config();
 const express = require("express")
 const path = require("path")
 const {logger} = require("./middleware/logEvents")
 const errorHandler = require("./middleware/errorHandler")
 const app = express()
 const cors = require("cors")
+const corsOptions = require("./config/corsOptions")
+const cookieParser = require("cookie-parser")
 const PORT = process.env.PORT || 8080
+const mongoose = require('mongoose')
+const connectDB = require('./config/dbConn')
 
-//generate req log
+//Connect to MongoDB
+connectDB();
+
+//Generate all request Log
 app.use(logger)
 
-const whitelist = ["http://127.0.0.1:3000","http://127.0.0.1:8080"]
-const corsOptions = {
-  origin: (origin,callback) => {
-    if(whitelist.indexOf(origin) !== -1 || !origin){
-      callback(null,true)
-    }else{
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  optionsSuccessStatus:200
-}
 
 app.use(cors(corsOptions))
 
+
 app.use(express.urlencoded({extended: false}))
+
+//Built-in middleware for json
 app.use(express.json())
-app.use(express.static(path.join(__dirname,'/public')))
+
+//Middleware for cookie
+app.use(cookieParser())
+
+app.use('/', (req,res) => {
+  res.send("This is the blog app api")
+})
 
 app.use(errorHandler)
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`)
+mongoose.connection.once('open', () =>{
+  console.log('Connected to MongoDB')
+  app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`)
+  })
 })
